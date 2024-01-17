1a7d612f3728337455f8104b7efe2d7d36c73fd7
abelgh91 <abelgh91@gmail.com>
2024-01-17 18:38:05 +0100

    nuevo proyecto tienda

---
+++ Proyectos-JavaScript/Tienda/src/producto.js
@@ -0,0 +1,15 @@
+const producto = document.getElementById('producto');
+const productoImagen = producto.querySelector('.producto__imagen');
+const thumbs = producto.querySelector('.producto__thumbs');
+
+thumbs.addEventListener('click', (e)=>{
+    if(e.target.tagName === 'IMG'){
+        const imagenSrc = e.target.src;
+        //Obtenemos la posicion del ultimo slash
+        const lastIndex = imagenSrc.lastIndexOf('/');
+        //Cortamos el string
+        const nombreImagen = imagenSrc.substring(lastIndex + 1);
+        //Cambiamos la ruta de la imagen principal
+        productoImagen.src = `./img/tennis/${nombreImagen}`
+    }
+})
